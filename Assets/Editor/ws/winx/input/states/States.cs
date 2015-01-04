b05ca3d961f52f70af496db2e954527f7b90fa33
xxx <winxalex@yahoo.com>
2015-01-04 18:04:44 +0100

    remove fault saving of states inside Editor

--- Assets/Editor/ws/winx/input/states/States.cs
+++
@@ -1,19 +0,0 @@
-﻿//THIS IS AUTO-GENERATED FILE. DON'T CHANGE
-
-
-namespace ws.winx.input.states{
-   
-  public enum States:int{
-		Wave=1397315813,
-	MyCustomState=-1624475888,
-	ManualAddedSTATE=-977892492,
-	AnyJoystick=-1887939231,
-	WalkForward=-2035216068,
-	WalkBackward=2035415549,
-
-	
-  }
-  
-}
-		
-		
